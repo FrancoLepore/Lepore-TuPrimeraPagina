bf62482074795757a8284c5edbaac484f25d371d
Franco Lepore Cariolo <francoleporecariolo@gmail.com>
2025-04-26 15:36:44 -0300

    agrego la fecha de creacion del usuario, siendo este el campo a eleccion

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class InfoExtra(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to="avatares", null=True, blank=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
